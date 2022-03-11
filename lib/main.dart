5af6c2beaa671426d29e5935460058e0b4bf8936
RipatTan <ripatalsafar@gmail.com>
2022-03-11 14:32:43 +0700

    sliver app bar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_daily/widgets/choice_chip_widget.dart';
 import 'package:flutter_daily/widgets/hero_widget.dart';
+import 'package:flutter_daily/widgets/sliver_app_bar.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ChoiceChipWidget(),
+      home: SliverAppBarWidget(),
     );
   }
 }
