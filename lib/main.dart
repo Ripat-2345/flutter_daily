730d6edb075034d90b54582d1da7ad49b4b6eccc
RipatTan <ripatalsafar@gmail.com>
2022-03-15 10:44:58 +0700

    ExpansionTile widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_daily/widgets/choice_chip_widget.dart';
+import 'package:flutter_daily/widgets/expansion_tile_widget.dart';
 import 'package:flutter_daily/widgets/hero_widget.dart';
-import 'package:flutter_daily/widgets/sliver_app_bar.dart';
+import 'package:flutter_daily/widgets/sliver_app_bar_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SliverAppBarWidget(),
+      home: ExpansionTileWidget(),
     );
   }
 }
